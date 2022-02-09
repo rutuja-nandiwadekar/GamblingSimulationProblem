b8480bb41c7655f3709a3fd3a4d17abc5804caa5
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-09 12:34:52 +0530

    Added: initialization for code

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -4,5 +4,8 @@ public class GamblingSimulation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation Problem");
+
+		final int stake = 100;
+		final int bet = 1;
 	}
 }
