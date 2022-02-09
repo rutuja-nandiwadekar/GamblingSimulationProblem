8da2b7993b78e56552cd9456410ef20c8dd9533f
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-09 13:27:07 +0530

    Added: Win or loose logic

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -1,16 +1,17 @@
 package com.bridgelabz;
 
 public class GamblingSimulation {
-	//initialization
+	// initialization
 	public static int STAKE = 100;
 	public static int BET = 1;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation Problem");
-		winOrloose(); //calling method
+		winOrloose(); // calling method
 	}
 
 	public static void winOrloose() {
-		//initialization
+
 		int check = (int) Math.floor((Math.random() * 10) % 2);
 		if (check == 0) {
 			STAKE++;
@@ -19,5 +20,6 @@ public class GamblingSimulation {
 			STAKE--;
 			System.out.println("Gambler loose");
 		}
+		System.out.println("stake value is: " +STAKE);
 	}
 }
